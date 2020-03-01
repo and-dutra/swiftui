9c92cc0aedaa5c483dd0b0e10ae0d99d139a1721
André Dutra <adoliveira@protonmail.com>
2020-03-01 16:41:34 -0300

    initial commit

---
+++ Landmarks-ios/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by André on 01/03/20.
+//  Copyright © 2020 André. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle()
+            .stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
